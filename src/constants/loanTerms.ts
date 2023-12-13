427ef3fec775d7ec39eea3796ac326ebf128adf8
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-13 15:44:59 -0600

    feat: a constant with the values of the loan combobox.

---
+++ src/constants/loanTerms.ts
@@ -0,0 +1,8 @@
+const loanTerms = [
+  { value: '30', label: '30 years' },
+  { value: '20', label: '20 years' },
+  { value: '15', label: '15 years' },
+  { value: '10', label: '10 years' },
+]
+
+export default loanTerms
