e870bc1e31576bceddc7bd3829a0abce9405f212
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-13 17:29:32 -0600

    test: creation of new tests

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -28,4 +28,32 @@ describe('morgagePayments function', () => {
 
     expect(result.toFixed(2)).toBe('1109.53')
   })
+
+  it('should calculate monthly payment correctly for a 30-year loan at 2%', () => {
+    const principalLoanAmount = 125000
+    const yearlyInterestRatePercentage = 2
+    const loanTermInYears = 15
+
+    const result = mortgagePayments(
+      principalLoanAmount,
+      yearlyInterestRatePercentage,
+      loanTermInYears
+    )
+
+    expect(result.toFixed(2)).toBe('804.39')
+  })
+
+  it('should calculate monthly payment correctly for a 15-year loan at 2%', () => {
+    const principalLoanAmount = 225000
+    const yearlyInterestRatePercentage = 7
+    const loanTermInYears = 10
+
+    const result = mortgagePayments(
+      principalLoanAmount,
+      yearlyInterestRatePercentage,
+      loanTermInYears
+    )
+
+    expect(result.toFixed(2)).toBe('2612.44')
+  })
 })
