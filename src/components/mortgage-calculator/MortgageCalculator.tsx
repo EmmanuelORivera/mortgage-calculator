427b8c4afa4984c54ad9ac6d972590e6f6603bf8
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-13 18:17:07 -0600

    feat: useEffect to change the value

--- src/components/mortgage-calculator/MortgageCalculator.tsx
+++ src/components/mortgage-calculator/MortgageCalculator.tsx
@@ -1,13 +1,37 @@
 'use client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Label } from '../ui/label'
 import { Input } from '../ui/input'
 import ComboBox from '../ui/combobox'
 import loanTerms from '@/constants/loanTerms'
+import { mortgagePayments } from '@/lib/utils'
 
 const MortgageCalculator = () => {
   const [selectedLoanTerm, setSelectedLoanTerm] = useState('')
+  const [principalLoan, setPrincipalLoan] = useState('')
+  const [annualInterestRate, setAnnualInterestRate] = useState('')
+  const [monthlyPayment, setMonthlyPayment] = useState(0)
 
+  useEffect(() => {
+    if (selectedLoanTerm && principalLoan && annualInterestRate) {
+      const monthlyPayment = mortgagePayments(
+        Number(principalLoan),
+        Number(annualInterestRate),
+        Number(selectedLoanTerm)
+      )
+
+      setMonthlyPayment(monthlyPayment)
+    }
+  }, [selectedLoanTerm, principalLoan, annualInterestRate])
+
+  const handleAnnualInterestRateChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const value = e.target.value
+    if (value === '' || (Number(value) >= 0 && Number(value) <= 100)) {
+      setAnnualInterestRate(value)
+    }
+  }
   return (
     <section>
       <h2 className="text-2xl my-4 font-bold">Mortgage Calculator</h2>
@@ -18,16 +42,20 @@ const MortgageCalculator = () => {
         id="loan"
         className="mt-1"
         placeholder="Enter Principal Loan Amount"
+        value={principalLoan}
+        onChange={(e) => setPrincipalLoan(e.target.value)}
         type="number"
       />
 
       <Label htmlFor="interestRate" className="font-semibold">
-        Anual Interest Rate
+        Annual Interest Rate (%)
       </Label>
       <Input
         id="interestRate"
         className="mt-1"
-        placeholder="Enter Anual Interest Rate"
+        placeholder="Enter Annual Interest Rate"
+        value={annualInterestRate}
+        onChange={handleAnnualInterestRateChange}
         type="number"
       />
 
@@ -42,6 +70,10 @@ const MortgageCalculator = () => {
         value={selectedLoanTerm}
         setValue={setSelectedLoanTerm}
       />
+
+      <p className="uppercase tracking-wider mt-8 text-right">
+        <span className="font-bold">Monthly Payment:</span> ${monthlyPayment}
+      </p>
     </section>
   )
 }
