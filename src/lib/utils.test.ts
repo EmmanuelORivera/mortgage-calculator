9e3a0b0ee5103b5c4049938f98d49f766f5003c0
Emmanuel Ochoa <eorivera95@gmail.com>
2023-12-13 18:16:38 -0600

    refactor: typo

--- src/lib/utils.test.ts
+++ src/lib/utils.test.ts
@@ -3,12 +3,12 @@ import { mortgagePayments } from './utils'
 describe('morgagePayments function', () => {
   it('should calculate monthly payment correctly for a 30-year loan at 5%', () => {
     const principalLoanAmount = 100000
-    const yearlyInterestRatePercentage = 5
+    const annualInterestRatePercentage = 5
     const loanTermInYears = 30
 
     const result = mortgagePayments(
       principalLoanAmount,
-      yearlyInterestRatePercentage,
+      annualInterestRatePercentage,
       loanTermInYears
     )
 
@@ -17,12 +17,12 @@ describe('morgagePayments function', () => {
 
   it('should calculate monthly payment correctly for a 15-year loan at 4%', () => {
     const principalLoanAmount = 150000
-    const yearlyInterestRatePercentage = 4
+    const annualInterestRatePercentage = 4
     const loanTermInYears = 15
 
     const result = mortgagePayments(
       principalLoanAmount,
-      yearlyInterestRatePercentage,
+      annualInterestRatePercentage,
       loanTermInYears
     )
 
@@ -31,12 +31,12 @@ describe('morgagePayments function', () => {
 
   it('should calculate monthly payment correctly for a 30-year loan at 2%', () => {
     const principalLoanAmount = 125000
-    const yearlyInterestRatePercentage = 2
+    const annualInterestRatePercentage = 2
     const loanTermInYears = 15
 
     const result = mortgagePayments(
       principalLoanAmount,
-      yearlyInterestRatePercentage,
+      annualInterestRatePercentage,
       loanTermInYears
     )
 
@@ -45,12 +45,12 @@ describe('morgagePayments function', () => {
 
   it('should calculate monthly payment correctly for a 15-year loan at 2%', () => {
     const principalLoanAmount = 225000
-    const yearlyInterestRatePercentage = 7
+    const annualInterestRatePercentage = 7
     const loanTermInYears = 10
 
     const result = mortgagePayments(
       principalLoanAmount,
-      yearlyInterestRatePercentage,
+      annualInterestRatePercentage,
       loanTermInYears
     )
 
